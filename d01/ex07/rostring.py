d5be853dfd392343183d74fdaebe60b1f8a26e8f
erikagreen7777 <erikagreen7777@gmail.com>
2018-02-20 14:45:20 -0800

    TODO: fix argc issues

---
+++ d01/ex07/rostring.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import sys
+
+
+def ft_split(string):
+    array = string.split(" ")
+    j = 0
+    for i in range(0, len(array)):
+        if array[i] != '':
+            j += 1
+
+    string = [None] * j
+    j = 0
+    for i in range(0, len(array)):
+        if array[i] != '':
+            string[j] = array[i]
+            i += 1
+            j += 1
+    return (string)
+#
+#
+def main():
+    if len(sys.argv) > 1:
+        # TODO: fix sdfkjssldkfj asldkfjdlfkjsdlfkjs
+        new = ft_split(sys.argv[1])
+        print ' '.join(new[1:] + new[:1])
+    else:
+        print " "
+
+
+if __name__ == "__main__":
+    main()
