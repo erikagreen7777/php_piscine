c5c4284e2cf082df1733973eefdf0688654d7540
erikagreen7777 <erikagreen7777@gmail.com>
2018-02-21 08:50:16 -0800

    one_more time

---
+++ d02/ex01/one_more_time.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import sys
+import calendar
+
+def main():
+    if len(sys.argv) > 1:
+        array = ' '.join(sys.argv[1:])
+        print array
+
+
+
+
+
+if __name__ == "__main__":
+    main()
